b35a018812691d43ea9d306eee91ec7c0b473eeb
doesitworkornot <60290354+doesitworkornot@users.noreply.github.com>
2021-07-30 14:54:04 +0800

    user add almost complete

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -64,15 +64,46 @@ async def add_user(m: Message, state: FSMContext):
 
 
 async def new_user_id(m: Message, state: FSMContext):
-    print(m)
     if m.forward_from != None:
         new_id = m.forward_from.id
-        await m.bot.send_message(text=f'You gonna add user wit id: {new_id}.', chat_id=m.chat.id)
+        await  state.update_data(new_id=new_id)
+        await m.bot.send_message(text=f'You gonna add user wit id: {new_id}. Send me user name', chat_id=m.chat.id)
+        await Admin_adding_user.user_name.set()
     elif m.text.isdigit():
         new_id = m.text
-        await m.bot.send_message(text=f'You gonna add user wit id: {new_id}.', chat_id=m.chat.id)
+        await  state.update_data(new_id=new_id)
+        await m.bot.send_message(text=f'You gonna add user wit id: {new_id}. Send me user name', chat_id=m.chat.id)
+        await Admin_adding_user.user_name.set()
     else:
         await m.bot.send_message(text='You failed. Try again. Or new user could set private forwarded message in settings, so ask him to send you his id', chat_id=m.chat.id)
+
+
+async def new_user_name(m: Message, state: FSMContext):
+    new_name = m.text
+    await state.update_data(new_name=new_name)
+    await m.bot.send_message(text='Its pretty isnt it? Next step is new users role', chat_id=m.chat.id)
+    await Admin_adding_user.user_role.set()
+
+
+async def new_user_role(m: Message, state: FSMContext):
+    new_role = m.text
+    await state.update_data(new_role=new_role)
+    await m.reply('Ok. Thats great, but are you sure?',reply_markup=kb.inline_kb_user_full)
+    await Admin_adding_user.confirm.set()
+
+
+async def new_user_confirm(m: Message, state: FSMContext):
+    data = await state.get_data()
+    new_id = data.get('new_id')
+    new_name = data.get('new_name')
+    new_role = data.get('new_role')
+    await m.bot.send_message(text=f'Ok, {new_name} with id: {new_id} and role: {new_role} will be added', chat_id = m.message.chat.id)
+    await state.finish()
+
+
+async def new_user_decline(m: Message, state: FSMContext):
+    await m.bot.send_message(text='Allright, operation aborted.', chat_id = m.message.chat.id)
+    await state.finish()
 
 
 async def del_user(m: Message):
@@ -122,9 +153,17 @@ def register_admin(dp: Dispatcher):
     ### Adding User Handlers
     dp.register_message_handler(new_user_id, state=Admin_adding_user.user_id,
                                 role=UserRole.ADMIN)
+    dp.register_message_handler(new_user_name,
+                                state=Admin_adding_user.user_name,
+                                role=UserRole.ADMIN)
+    dp.register_message_handler(new_user_role,
+                                state=Admin_adding_user.user_role,
+                                role=UserRole.ADMIN)
 
     ### Callbacks
     dp.register_callback_query_handler(print_q, text=['print'],
                                        role=UserRole.ADMIN)
     dp.register_callback_query_handler(info, text=['info'],
                                        role=UserRole.ADMIN)
+    dp.register_callback_query_handler(new_user_confirm, text=['user_add_confirm'], state = Admin_adding_user.confirm, role=UserRole.ADMIN)
+    dp.register_callback_query_handler(new_user_decline, text=['user_add_decline'], state = Admin_adding_user.confirm, role=UserRole.ADMIN)
--- tgbot/handlers/kb.py
+++ tgbot/handlers/kb.py
@@ -9,3 +9,8 @@ inline_kb1 = InlineKeyboardMarkup().add(inline_btn_print)
 inline_kb_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_print)
 inline_kb_full.add(InlineKeyboardButton('Id like to know smth about my account',
                                         callback_data='info'))
+
+                                        
+inline_btn_user_confirm = InlineKeyboardButton('Yes, im sure', callback_data = 'user_add_confirm')
+inline_kb_user_full = InlineKeyboardMarkup(row_width=2).add(inline_btn_user_confirm)
+inline_kb_user_full.add(InlineKeyboardButton('No, just a miss click', callback_data='user_add_decline'))
