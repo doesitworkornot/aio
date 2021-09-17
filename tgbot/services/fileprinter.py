ff66758ff510b3043cb0a0bfe356e70399a16197
doesitworkornot <60290354+doesitworkornot@users.noreply.github.com>
2021-09-17 20:31:42 +0800

    printing pages

--- tgbot/services/fileprinter.py
+++ tgbot/services/fileprinter.py
@@ -5,6 +5,7 @@ from tgbot.config import load_config
 
 import logging
 import urllib
+import subprocess
 
 
 allowedfiles = ['jpg','png', 'pdf', 'tiff', 'cdr', 'psd', 'ai', 'bmp', 'gif', 'odf', 'odt', 'doc', 'docx', 'jpeg']
@@ -18,7 +19,30 @@ async def download(state, m):
 
     file = await m.bot.get_file(file_id)
     file_path = file.file_path
-    file_dest = f'/home/botuser/aiobot/docs/{m.message.chat.id}{file_id}.{file_extension}'
-    token = 'SOME RANDOM SYMBOLS'
+    token = 'mr kostyl'
     link = f'https://api.telegram.org/file/bot{token}/{file_path}'
-    urllib.request.urlretrieve(link, file_dest)
+    file_name = f'{m.message.chat.id}{file_id}.{file_extension}'
+
+    if file_extension == 'pdf':
+        file_dest = f'/home/botuser/aiobot/pdfs/{file_name}'
+        urllib.request.urlretrieve(link, file_dest)
+    else:
+        file_dest = f'/home/botuser/aiobot/docs/{file_name}'
+        urllib.request.urlretrieve(link, file_dest)
+        await convert(file_dest)
+    file_dest = f'/home/botuser/aiobot/pdfs/{file_name}'
+    if int(copies) > 1:
+        cmd = ['lp', '-n', copies, file_dest]
+    else:
+        cmd = ['lp', file_dest]
+    traceback = subprocess.run(cmd)
+    if traceback.returncode == 0:
+        return 0
+
+
+
+
+async def convert(file_dest):
+    cmd = ['lowriter', '--convert-to', 'pdf', '--outdir', '/home/botuser/aiobot/pdfs', file_dest]
+    traceback = subprocess.run(cmd, check=True)
+    return traceback.returncode
